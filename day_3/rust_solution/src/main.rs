2c307c9cf2d667e46e435cee813d21097ec535c3
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-12-13 17:24:54 -0500

    update rust solution

--- day_3/rust_solution/src/main.rs
+++ day_3/rust_solution/src/main.rs
@@ -1,13 +1,12 @@
-#[derive(Debug)]
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone)]
 struct Part {
     row: usize,
     start_col: usize,
     end_col: usize,
     digits: usize,
 }
-
-fn compare_lines(parts: Vec<Part>, symbols: Vec<(usize, usize)>):
-
 
 fn parse_text(str_input: &str) -> (Vec<Part>, Vec<(usize, usize)>) {
     let mut current_digits: String = String::from("");
@@ -48,6 +47,65 @@ fn parse_text(str_input: &str) -> (Vec<Part>, Vec<(usize, usize)>) {
     });
 
     (parts, symbols)
+}
+
+fn get_parts(str_input: &str) -> Vec<Part> {
+    let mut current_digits: String = String::from("");
+    let mut parts: Vec<Part> = Vec::new();
+
+    let mut prev_symbols: HashSet<usize> = HashSet::new();
+    let mut curr_symbols: HashSet<usize> = HashSet::new();
+
+    let mut prev_parts: HashMap<usize, Vec<Part>> = HashMap::new();
+    let mut curr_parts: HashMap<usize, Vec<Part>> = HashMap::new();
+
+    let mut last_symbol_seen: Option<usize> = None;
+    let mut last_part_seen: Option<Part> = None;
+
+    str_input.lines().enumerate().for_each(|(row, line)| {
+        line.chars().enumerate().for_each(|(col, ch)| {
+            if ch.is_digit(10) {
+                current_digits.push(ch)
+            } else {
+                if ch != '.' {
+                    if col > 0 {
+                        curr_symbols.insert(col - 1);
+                    }
+                    curr_symbols.insert(col);
+                    curr_symbols.insert(col + 1);
+                }
+                if !current_digits.is_empty() {
+                    parts.push(Part {
+                        row,
+                        start_col: col - current_digits.len(),
+                        end_col: col - 1,
+                        digits: current_digits.parse().unwrap(),
+                    });
+                    current_digits.drain(..);
+                }
+            }
+        });
+
+        if !current_digits.is_empty() {
+            let col = line.len();
+            println!("{row}, {col}, {:?}", current_digits);
+            parts.push(Part {
+                row,
+                start_col: col - current_digits.len(),
+                end_col: col - 1,
+                digits: current_digits.parse().unwrap(),
+            });
+            current_digits.drain(..);
+        }
+
+        prev_symbols = curr_symbols.clone();
+        curr_symbols.drain();
+
+        prev_parts = curr_parts.clone();
+        curr_parts.drain();
+    });
+
+    parts
 }
 
 fn main() {
